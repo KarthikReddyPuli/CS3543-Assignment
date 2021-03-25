d9d098cdfbd34a3a837086168cd06aa8ea3e4230
KarthikReddyPuli <puli.karthikreddy@gmail.com>
2021-03-25 22:07:17 +0530

    multi thread working for small files

--- UDP_Client.py
+++ UDP_Client.py
@@ -12,6 +12,8 @@ import struct
 import sys
 import hashlib
 import select
+import threading
+import datetime
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
@@ -24,13 +26,14 @@ if len(sys.argv) >= 2:
 if len(sys.argv) >= 3:
     fileName = sys.argv[2]
 
+recvUpdate = threading.Event()
+recv_UDP_Packet = [-1]
 in_file = open(fileName, "rb")
 PData = None
 bytesCount = 8
 endMessage = b'complete'
-DataA = [b'NCC-1701', b'NCC-1664', b'NCC-1017',endMessage]
 data = ""
-print("UDP target IP:", UDP_IP)
+print("UDP target IP:", SERVER_IP)
 print("UDP target port:", UDP_PORT2)
 
 # Send the UDP Packet
@@ -41,86 +44,81 @@ sock2.bind((UDP_IP, UDP_PORT2))
 x=0
 end=0
 
-# Create a loop to send each mark
-while end == 0:
+def updateRecv():
+    global recv_UDP_Packet
+    UDP_Packet_Data = struct.Struct('I I 8s 32s')
+    while True:
+        sock2.recvfrom(1024)
+        recv_UDP_Packet = UDP_Packet_Data.unpack(PData)
+        recvUpdate.set()
 
-    print("Packet Number: " + str(x+1))
-    data = in_file.read(bytesCount)
-    print(data)
-    if data == b'' :
-        in_file.close()
-        data = endMessage
-        end = 1
-
+def sendData(count,data):
     while True:
 
         # Create the Checksum
-        values = (0, x % 2, data)
+        values = (count, count % 2, data)
         UDP_Data = struct.Struct('I I 8s')
         packed_data = UDP_Data.pack(*values)
         chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
 
         # Build the UDP Packet
-        values = (0, x % 2, data, chksum)
+        values = (count, count % 2, data, chksum)
 
-        print("Sending Packet: ")  # Send the packet before packing it
-        print(values)
+        #print("Sending Packet: ")  # Send the packet before packing it
+        #print(values)
 
         UDP_Packet_Data = struct.Struct('I I 8s 32s')
         UDP_Packet = UDP_Packet_Data.pack(*values)
 
         # Send Packet through
         sock.sendto(UDP_Packet, (SERVER_IP, UDP_PORT))
+        startTime = datetime.datetime.now()
+        timeout = 0
 
-        # Create a timer
-        timer = select.select([sock2], [], [], 0.009)
+        while recv_UDP_Packet[0] != UDP_Packet[0] :
+            recvUpdate.wait()
+            timeDifference = datetime.datetime.now() - startTime
+            if timeDifference.total_seconds() > 1 :
+                timeout = 1
+                break
 
-        # Check if data was sent
-        if timer[0]:
-            PData, addr = sock2.recvfrom(1024)
-        # If not,
-        else:
-            print("Timer Expired")
-            PData = None
+        if timeout == 1:
+            if recv_UDP_Packet[0] != UDP_Packet[0]:
+                print("Timeout")
+                continue
 
-        if PData is None:
-            print("data is none")
+        recvUpdate.clear()
+        values = (recv_UDP_Packet[0], recv_UDP_Packet[1], recv_UDP_Packet[2])
+        packer = struct.Struct('I I 8s')
+        packed_data = packer.pack(*values)
+        chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+        # Print the Data
+
+        #print(UDP_Packet)
+
+        # Check if data is corrupt
+        if UDP_Packet[3] != chksum:
+            print('Checksums Do Not Match, Packet Corrupt')
             continue
 
-        else:
-            print("Recieved: ")
+        # Check the sequence number.
+        if UDP_Packet[1] == (count+1) % 2:
+            print("Incorrect Sequence Number, \nPacket resending ...\n...", x)
+            continue
 
-            # Unpack Data
-            UDPRecv = UDP_Packet_Data.unpack(PData)
-            UDP_Packet = UDP_Packet_Data.unpack(PData)
-            values = (UDP_Packet[0], UDP_Packet[1], UDP_Packet[2])
-            packer = struct.Struct('I I 8s')
-            packed_data = packer.pack(*values)
-            chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
 
-            # Print the Data
+# Create a loop to send each mark
+while end == 0:
 
-            print(UDP_Packet)
+    print("Packet Number: " + str(x+1))
+    data = in_file.read(bytesCount)
+    if data == b'' :
+        in_file.close()
+        data = endMessage
+        end = 1
 
-            # Check if data is corrupt
-            if UDP_Packet[3] == chksum:
-                print('CheckSums Match')
-            else:
-                print('Checksums Do Not Match, Packet Corrupt')
-                continue
-
-            # Check the sequence number.
-            if UDP_Packet[1] == (x+1) % 2:
-                print("Incorrect Sequence Number, \nPacket resending ...\n...", x)
-                continue
-            else:
-                print("Sequence Number Correct")
-
-            # Check if data is Acknowledged
-            if UDP_Packet[0] == 1:
-                print('Packet is Acknowledged')
-                break
-            else:
-                print('Packet is not Acknowlegde')
+    thread = threading.Thread(target=sendData,args=(x,data,))
+    thread.start()
 
     x = x + 1
--- UDP_Server.py
+++ UDP_Server.py
@@ -13,6 +13,10 @@ import sys
 import hashlib
 import select
 import codecs
+import threading
+
+fileUpdate = threading.Event()
+endLoop = threading.Event()
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
@@ -37,6 +41,91 @@ sock.bind((UDP_IP, UDP_PORT))
 
 end = 0
 x = 0
+writeCount = 0
+
+def writeData(count,data):
+    global writeCount
+    if(writeCount > count):
+        return
+    while count != writeCount :
+        fileUpdate.wait()
+
+    fileUpdate.clear()
+    out_file.write(data)
+    writeCount = writeCount+1
+    fileUpdate.set()
+
+def closeFile(count):
+    if(writeCount > count):
+        return
+    endLoop.set()
+    while count != writeCount :
+        fileUpdate.wait()
+
+    out_file.close()
+    print("File closed")
+
+def receive_data(data,addr):
+
+    # Receive Data
+    UDP_Packet = unpacker.unpack(data)
+    #print("received from:", addr)
+    #print("received message:", UDP_Packet)
+
+    # Create the Checksum for comparison
+    values = (UDP_Packet[0], UDP_Packet[1], UDP_Packet[2])
+    packer = struct.Struct('I I 8s')
+    packed_data = packer.pack(*values)
+    chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+    # Compare Checksums to test for corrupt data
+    if UDP_Packet[3] == chksum and UDP_Packet[1] == UDP_Packet[0] % 2:
+        #print('CheckSums Match, Sequence Number is correct, Packet OK')
+
+        # Create the Checksum
+        values = (1, UDP_Packet[1], UDP_Packet[2])
+        UDP_Data = struct.Struct('I I 8s')
+        packed_data = UDP_Data.pack(*values)
+        chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+        # Build the UDP Packet
+        values = (UDP_Packet[0], UDP_Packet[1], UDP_Packet[2], chksum)
+
+        #print("Sending Packet: ")  # Print packet before packing data
+        #print(values)
+
+        UDP_Packet_Data = struct.Struct('I I 8s 32s')
+        UDP_Packet_send = UDP_Packet_Data.pack(*values)
+
+        # Send Packet through
+        sock2.sendto(UDP_Packet_send, (Client_IP, UDP_PORT2))
+
+        if UDP_Packet[2] == endMessage:
+            closeFile(UDP_Packet[0])
+        else:
+            offset = removeNullBytes(UDP_Packet[2])
+            writeData(UDP_Packet[0],UDP_Packet[2][:offset])
+
+    else:
+
+        print('CheckSums do not Match or the Sequence Number is incorrect, Packet is not ok')
+
+        # Create the Checksum
+        values = (1, (UDP_Packet[0]+1) % 2, UDP_Packet[2])
+        UDP_Data = struct.Struct('I I 8s')
+        packed_data = UDP_Data.pack(*values)
+        chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+        #print("Sending Packet: ")
+        #print(UDP_Packet)
+
+        # Build the UDP Packet
+        values = (1, (UDP_Packet[0] + 1) % 2, UDP_Packet[2], chksum)
+        UDP_Packet_Data = struct.Struct('I I 8s 32s')
+        UDP_Packet = UDP_Packet_Data.pack(*values)
+
+        # Send Packet through
+        sock2.sendto(UDP_Packet, (Client_IP, UDP_PORT2))
 
 def removeNullBytes(input):
     offset = 8
@@ -49,75 +138,13 @@ def removeNullBytes(input):
             break
     return offset
 
-while end == 0:
+while endLoop.is_set() == False:
 
     print("Packet Number: " + str(x+1))
-
-    while True:
-
-        # Receive Data
-        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-        UDP_Packet = unpacker.unpack(data)
-        print("received from:", addr)
-        print("received message:", UDP_Packet)
-
-        # Create the Checksum for comparison
-        values = (UDP_Packet[0], UDP_Packet[1], UDP_Packet[2])
-        packer = struct.Struct('I I 8s')
-        packed_data = packer.pack(*values)
-        chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
-
-        # Compare Checksums to test for corrupt data
-        if UDP_Packet[3] == chksum and UDP_Packet[1] == x % 2:
-            print('CheckSums Match, Sequence Number is correct, Packet OK')
-
-            # Create the Checksum
-            values = (1, UDP_Packet[1], UDP_Packet[2])
-            UDP_Data = struct.Struct('I I 8s')
-            packed_data = UDP_Data.pack(*values)
-            chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
-
-            # Build the UDP Packet
-            values = (1, UDP_Packet[1], UDP_Packet[2], chksum)
-
-            print("Sending Packet: ")  # Print packet before packing data
-            print(values)
-
-            if UDP_Packet[2] == endMessage:
-                end = 1
-                out_file.close()
-            else:
-                offset = removeNullBytes(UDP_Packet[2])
-                out_file.write(UDP_Packet[2][:offset])
-
-            UDP_Packet_Data = struct.Struct('I I 8s 32s')
-            UDP_Packet = UDP_Packet_Data.pack(*values)
-
-            # Send Packet through
-            sock2.sendto(UDP_Packet, (Client_IP, UDP_PORT2))
-            break
-
-        else:
-
-            print('CheckSums do not Match or the Sequence Number is incorrect, Packet is not ok')
-
-            # Create the Checksum
-            values = (1, (x+1) % 2, UDP_Packet[2])
-            UDP_Data = struct.Struct('I I 8s')
-            packed_data = UDP_Data.pack(*values)
-            chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
-
-            print("Sending Packet: ")
-            print(UDP_Packet)
-
-            # Build the UDP Packet
-            values = (1, (x + 1) % 2, UDP_Packet[2], chksum)
-            UDP_Packet_Data = struct.Struct('I I 8s 32s')
-            UDP_Packet = UDP_Packet_Data.pack(*values)
-
-            # Send Packet through
-            sock2.sendto(UDP_Packet, (Client_IP, UDP_PORT2))
-
+    data, addr = sock.recvfrom(1024)
+    #receive_data(data,addr)
+    thread = threading.Thread(target=receive_data,args=(data,addr,))
+    thread.start()
     x = x+1
 
 
